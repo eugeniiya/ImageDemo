ae2f2d9e0b01512dd10787282a032b0011c6d9bb
eugeniiya <evgenia.fonina.2013@yandex.ru>
2021-12-04 18:32:42 +0300

    Добавьте файлы проекта.

---
+++ ImageDemo/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace ImageDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            var xamlTab = new TabbedPage();
+            xamlTab.Children.Add(new LocalImages { Title = "Local" });
+            xamlTab.Children.Add(new DownloadImages
+            { Title = "Downloaded" });
+            xamlTab.Children.Add(new EmbImages
+            { Title = "Embedded" });
+            xamlTab.Children.Add(new GIFImages
+            { Title = "GIF Files" });
+            MainPage = xamlTab;
+        }
+    }
+}
---
+++ ImageDemo/EmbImages.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Reflection;
+
+
+namespace ImageDemo
+{
+    public class EmbImages : ContentPage
+    {
+        public EmbImages()
+        {
+            var EmbedImage = new Image { Aspect = Aspect.Fill };
+            EmbedImage.Source = ImageSource.FromResource(
+            "ImageDemo.MyImages.beach.jpg",
+            typeof(EmbImages).GetTypeInfo().Assembly);
+            EmbedImage.HeightRequest = 350;
+            BackgroundColor = Color.Gold;
+            Content = new StackLayout
+            {
+                Children = {
+new Label {
+ Text = "Изображение как присоединенный ресурс",
+FontSize = 40,
+TextColor=Color.Black,
+FontAttributes = FontAttributes.Bold,
+HorizontalOptions = LayoutOptions.Center,
+ HorizontalTextAlignment=TextAlignment.Center
+},
+EmbedImage
+},
+                Margin = new Thickness(20, 50, 20, 20),
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand
+            };
+        }
+    }
+}
